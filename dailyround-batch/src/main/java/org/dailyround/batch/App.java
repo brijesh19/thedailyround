90f396172dd4739e4c12e7950063dd8199a7b522
Brijesh Yadav <brijeshapril@gmail.com>
2018-05-13 02:39:20 +0530

    The Daily Round: Initial setup

---
+++ dailyround-batch/src/main/java/org/dailyround/batch/App.java
@@ -0,0 +1,13 @@
+package org.dailyround.batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
